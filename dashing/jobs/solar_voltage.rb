ff9c01c554e071640a08c4238f89e715dfcba59c
Brandon Withrow <buba447@gmail.com>
2018-05-12 08:58:30 -0500

    Added another widget

--- dashing/jobs/solar_voltage.rb
+++ dashing/jobs/solar_voltage.rb
@@ -6,28 +6,45 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
   db = Mysql2::Client.new(:host => "localhost", :username => "buba", :password => "buba", :database => "van_solar" )
 
   # Mysql query
-  sql = "select solar_voltage, UNIX_TIMESTAMP(date) FROM solar_voltage WHERE date >= NOW() - INTERVAL 1 DAY"
+  voltageSql = "select solar_voltage, UNIX_TIMESTAMP(date) FROM solar_voltage WHERE date >= NOW() - INTERVAL 1 DAY"
 
   # Execute the query
-  results = db.query(sql)
-  puts results
+  voltageResults = db.query(voltageSql)
 
   # Sending to List widget, so map to :label and :value
-  voltageItems = results.map do |row|
+  voltageItems = voltageResults.map do |row|
     row = {
       :x => row['date'],
       :y => row['solar_voltage']
     }
   end
 
+  # Mysql query
+  ampsSql = "select solar_amps, UNIX_TIMESTAMP(date) FROM solar_amps WHERE date >= NOW() - INTERVAL 1 DAY"
+
+  # Execute the query
+  ampsResults = db.query(ampsSql)
+
+  # Sending to List widget, so map to :label and :value
+  ampItems = ampsResults.map do |row|
+    row = {
+      :x => row['date'],
+      :y => row['solar_amps']
+    }
+  end
+
+  series = [
+    {
+        name: "Volts",
+        data: voltageItems
+    },
+    {
+        name: "Amps",
+        data: ampItems
+    }
+  ]
+
   # Update the List widget
-  send_event(:solar_voltage, points: voltageItems)
+  send_event('pv_power', points: [points1, points2])
 
 end
-
-# def data_with_minmax(nested_array)
-#   out = nested_array.first[:datapoints].collect {|ind| ind[0]}.minmax
-#   {series: nested_array, min: out[0]-1, max: out[1]+1 }
-# end
-
-# send_event("open_tickets"  , data_with_minmax(graphite_data) )
