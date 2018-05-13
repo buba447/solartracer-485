f7a05723be6cafd7270d08779b81a99249ae44d5
Brandon Withrow <buba447@gmail.com>
2018-05-13 17:52:55 -0500

    update

--- dashing/jobs/getRegister.py
+++ dashing/jobs/getRegister.py
@@ -2,27 +2,27 @@
 import sys
 import minimalmodbus
 
+def readRegister(register):
+	try:
+		instrument = minimalmodbus.Instrument('/dev/ttyXRUSB0', 1)
+	except minimalmodbus.serial.SerialException:
+		return 0
+
+	instrument.serial.baudrate = 115200
+	instrument.serial.bytesize = 8
+	instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
+	instrument.serial.stopbits = 1
+	instrument.serial.timeout  = 1.2
+	instrument.mode = minimalmodbus.MODE_RTU
+
+	try:
+		reading = instrument.read_register(address, 2, 4)
+		return reading
+	except IOError:
+		return 0
+
 if len(sys.argv) == 1:
 	print(0)
-	sys.exit()
-
-register = sys.argv[1]
-
-try:
-	instrument = minimalmodbus.Instrument('/dev/ttyXRUSB0', 1)
-except: minimalmodbus.serial.SerialException:
-	print(0)
-	sys.exit()
-
-instrument.serial.baudrate = 115200
-instrument.serial.bytesize = 8
-instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
-instrument.serial.stopbits = 1
-instrument.serial.timeout  = 1.2
-instrument.mode = minimalmodbus.MODE_RTU
-
-try:
-	reading = instrument.read_register(address, 2, 4)
-	print(reading)
-except IOError:
-	print(0)
+else :
+	register = sys.argv[1]
+	print readRegister(register)
