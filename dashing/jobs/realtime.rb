b64e34c9b16dbe3a7be47548d929deafe13ceaf6
Brandon Withrow <buba447@gmail.com>
2018-05-13 17:59:29 -0500

    update

--- dashing/jobs/realtime.rb
+++ dashing/jobs/realtime.rb
@@ -2,16 +2,16 @@ require 'mysql2'
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
 
-voltage = exec("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3104")
-solarAmp = exec("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3101")
-loadAmp = exec("python /home/pi/coding/white_whale/jobs/getRegister.py 0x310D")
-powerGen = exec("python /home/pi/coding/white_whale/jobs/getRegister.py 0x330C")
-powerUsed = exec("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3304")
+voltage = system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3104")
+solarAmp = system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3101")
+loadAmp = system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x310D")
+powerGen = system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x330C") / 12.0
+powerUsed = system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3304") / 12.0
 
 # Update the List widget
-send_event('battery_voltage', { value: rand(15) })
-send_event('power_gen', { current: rand(15) })
-send_event('power_used', { current: rand(15) })
-send_event('solar_amp', { current: rand(100) })
-send_event('load_amp', { current: rand(100) })
+send_event('battery_voltage', { value: voltage })
+send_event('power_gen', { current: powerGen })
+send_event('power_used', { current: powerUsed })
+send_event('solar_amp', { current: solarAmp })
+send_event('load_amp', { current: loadAmp })
 end
