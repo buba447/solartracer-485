1410a46539772b593a3a46a04c7e2f8593084ca4
Brandon Withrow <buba447@gmail.com>
2018-05-13 17:39:35 -0500

    Updated python script

--- dashing/jobs/realtime.rb
+++ dashing/jobs/realtime.rb
@@ -2,11 +2,11 @@ require 'mysql2'
 
 SCHEDULER.every '5s', :first_in => 0 do |job|
 
-voltage = exec("python getRegister.py 0x3104")
-solarAmp = exec("python getRegister.py 0x3101")
-loadAmp = exec("python getRegister.py 0x310D")
-powerGen = exec("python getRegister.py 0x330C")
-powerUsed = exec("python getRegister.py 0x3304")
+voltage = exec("python ~/coding/white_whale/jobs/getRegister.py 0x3104")
+solarAmp = exec("python ~/coding/white_whale/jobs/getRegister.py 0x3101")
+loadAmp = exec("python ~/coding/white_whale/jobs/getRegister.py 0x310D")
+powerGen = exec("python ~/coding/white_whale/jobs/getRegister.py 0x330C")
+powerUsed = exec("python ~/coding/white_whale/jobs/getRegister.py 0x3304")
 
 # Update the List widget
 send_event('battery_voltage', { value: rand(15) })
