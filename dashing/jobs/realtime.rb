fcc3db29d1ecd220a77fa28f17a00591f7b8cf69
Brandon Withrow <buba447@gmail.com>
2018-05-13 18:08:40 -0500

    update

--- dashing/jobs/realtime.rb
+++ dashing/jobs/realtime.rb
@@ -3,10 +3,10 @@ require 'mysql2'
 SCHEDULER.every '5s', :first_in => 0 do |job|
 
 voltage = (`python /home/pi/coding/white_whale/jobs/getRegister.py 0x3104`).to_f
-solarAmp = (`system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3101`).to_f
-loadAmp = (`system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x310D`).to_f
-powerGen = (`system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x330C`).to_f / 12.0
-powerUsed = (`system("python /home/pi/coding/white_whale/jobs/getRegister.py 0x3304`).to_f / 12.0
+solarAmp = (`python /home/pi/coding/white_whale/jobs/getRegister.py 0x3101`).to_f
+loadAmp = (`python /home/pi/coding/white_whale/jobs/getRegister.py 0x310D`).to_f
+powerGen = (`python /home/pi/coding/white_whale/jobs/getRegister.py 0x330C`).to_f / 12.0
+powerUsed = (`python /home/pi/coding/white_whale/jobs/getRegister.py 0x3304`).to_f / 12.0
 # Update the List widget
 send_event('battery_voltage', { value: voltage })
 send_event('power_gen', { current: powerGen })
