e85fa95ef9d23258993557347a0cb7315aaadd9d
Brandon Withrow <buba447@gmail.com>
2018-05-13 17:41:58 -0500

    update

--- dashing/jobs/getRegister.py
+++ dashing/jobs/getRegister.py
@@ -8,7 +8,12 @@ if len(sys.argv) == 1:
 
 register = sys.argv[1]
 
-instrument = minimalmodbus.Instrument('/dev/ttyXRUSB0', 1)
+try:
+	instrument = minimalmodbus.Instrument('/dev/ttyXRUSB0', 1)
+except: minimalmodbus.serial.SerialException:
+	print(0)
+	sys.exit()
+
 instrument.serial.baudrate = 115200
 instrument.serial.bytesize = 8
 instrument.serial.parity   = minimalmodbus.serial.PARITY_NONE
